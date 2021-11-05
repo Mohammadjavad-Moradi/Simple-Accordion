f011976954452966964fe0109d5f8db171145c6e
mohammadjavad-moradi <mr.mj.moradi@gmail.com>
2021-11-05 20:43:50 +0300

    unit test for header component

---
+++ src/Components/Header/Header.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import Header from './Header.component';
+describe('header functionality', () => {
+  test('login button exist when user is null, go to the login page when clicked', () => {
+    render(<Header user={null} />, { wrapper: Router });
+    const loginButton = screen.getByRole('link', { name: 'login page' });
+    expect(loginButton).toBeInTheDocument;
+
+    userEvent.click(loginButton);
+    expect(window.location.pathname).toBe('/login');
+  });
+
+  test('adviserButton exist when user logged in and go to adviser page on click', () => {
+    render(<Header user="mohammad" />, { wrapper: Router });
+    const adviserButton = screen.getByRole('link', { name: 'adviser page' });
+    expect(adviserButton).toBeInTheDocument;
+    userEvent.click(adviserButton);
+    expect(window.location.pathname).toBe('/adviser');
+  });
+});
